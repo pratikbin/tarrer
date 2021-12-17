a5b4e87ccf85f40abe1587343b5ecdc017086b4e
Pratik Balar <pratikbalar123@gmail.com>
2021-12-17 12:27:03 +0530

    feat: Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tarrer
+
 extract possibly any archive supported br, bz2, zip, gz, lz4, sz, xz, zstd
+
+
+## Run
+
+`go run main.go <archive path> <dest dir path>`
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/mholt/archiver/v3"
+)
+
+func main() {
+	if err := archiver.Unarchive(os.Args[1], os.Args[2]); err != nil {
+		log.Fatal(err)
+	}
+}
