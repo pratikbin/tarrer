9de49b6435d2a64ddd82e4f96484e8e910a1d2d7
Pratik Balar <pratikbalar123@gmail.com>
2021-12-19 00:34:52 +0530

    feat: add basic args condition

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,6 +9,10 @@ import (
 )
 
 func main() {
+	if len(os.Args) <= 1 || len(os.Args) > 2 {
+		fmt.Println("Usage: tarrer <archive path> <dest dir>")
+		os.Exit(1)
+	}
 	if err := archiver.Unarchive(os.Args[1], os.Args[2]); err != nil {
 		log.Fatal(err)
 	}
